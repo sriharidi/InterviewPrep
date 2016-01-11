7f302895cb20a3bba4495ea463b08add41d7be1a
sriharidi <srihari.busam@gmail.com>
2016-01-10 20:12:02 -0800

    added tree class template

---
+++ TreeProblems/Tree.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeProblems
+{
+    public class Tree
+    {
+        private TreeNode root;
+
+        public Tree(TreeNode root)
+        {
+            this.root = root;
+        }
+
+        public void InorderTraversal()
+        {
+        }
+
+        public void PreorderTraversal()
+        {
+        }
+
+        public void PostorderTraversal()
+        {
+        }
+    }
+}
