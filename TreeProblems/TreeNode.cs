463db217b7d0443f7849b993895561e3a8232428
sriharidi <srihari.busam@gmail.com>
2016-01-10 19:26:59 -0800

    added treenode file

---
+++ TreeProblems/TreeNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeProblems
+{
+    public class TreeNode
+    {
+        public int Data;
+        public TreeNode Left;
+        public TreeNode Right;
+
+        public TreeNode(int val)
+        {
+            this.Data = val;
+        }
+    }
+}
